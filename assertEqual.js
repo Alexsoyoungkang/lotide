a38afff5ea58d556d27e680e63079f841ccef986
Soyoung <ksyoung3424@gmail.com>
2023-04-21 12:58:02 +0000

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+//Define the assertEqual function and ensure that it can be called.
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+    //this can be used - console.log("Assertion Passed " + actual + " === " + expected);
+  } else {
+    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
+    // = console.log("Assertion Passed " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
