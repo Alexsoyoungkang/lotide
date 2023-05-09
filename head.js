27af69763ca234be9b19157a1c6e4cb50d51e551
Soyoung <ksyoung3424@gmail.com>
2023-05-09 03:58:55 +0000

    refactored head.js

--- head.js
+++ head.js
@@ -1,11 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+//const assertEqual = require('./assertEqual');
 
 
 const head = function(array) {
@@ -15,8 +15,11 @@ const head = function(array) {
   return array[0];
 };
 
+
+module.exports = head;
+
 // actual = head([5,6,7]), expected = 5
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([2]), 2);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
+// assertEqual(head([2]), 2);
