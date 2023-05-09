cef3d7353a91feb5b058d745b7aa698704fea824
Soyoung <ksyoung3424@gmail.com>
2023-05-09 04:00:45 +0000

    refactored head.js

--- head.js
+++ head.js
@@ -1,13 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-//const assertEqual = require('./assertEqual');
-
-
 const head = function(array) {
   if (array.length === 0) {
     return undefined;
@@ -18,8 +8,3 @@ const head = function(array) {
 
 module.exports = head;
 
-// actual = head([5,6,7]), expected = 5
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), undefined);
-// assertEqual(head([2]), 2);
